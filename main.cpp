9f7faae55030f4dba8f645ddd8fe58a9ae79adf7
Sergio Borghese <s.borghese@netresults.it>
2018-10-09 22:21:49 +0200

    Added thread name

--- main.cpp
+++ main.cpp
@@ -8,11 +8,20 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    qDebug() << "Look Ma' a Thread... " << QThread::currentThreadId();
+    // naming threads
+    QThread::currentThread()->setObjectName("T_Main");
+    qDebug() << "Look Ma' a Thread... " <<
+                QThread::currentThreadId() <<
+                QThread::currentThread()->objectName();
 
+    // create another thread
     QThread anotherThread;
+    anotherThread.setObjectName("T_AnotherThread");
+    // start the thread
     anotherThread.start();
-    qDebug() << "Look Ma' another Thread... " << anotherThread.currentThreadId();
+    qDebug() << "Look Ma' another Thread... " <<
+                anotherThread.currentThreadId() <<
+                anotherThread.objectName();
     return a.exec();
 }
 
