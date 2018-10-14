c1fff829721aa09bbef32a53794c183bc699b757
Sergio Borghese <s.borghese@netresults.it>
2018-10-15 00:18:56 +0200

    one thread two workers async

--- worker.h
+++ worker.h
@@ -6,12 +6,15 @@
 #include <QThread>
 #include <QDebug>
 #include <QDateTime>
+#include <QTimer>
 
 class Worker : public QObject
 {
     Q_OBJECT
 
     QString m_workerName;
+    QTimer* m_timerPtr;
+    qint32 m_counter;
 
 signals:
     void workerFinishedSignal();
@@ -22,21 +25,38 @@ public slots:
         qDebug() << QString("[%1][%2] start() method")
                   .arg(QThread::currentThread()->objectName())
                   .arg(m_workerName);
-        for (int i = 0; i < 10; i++)
+
+        m_timerPtr = new QTimer(this);
+        m_timerPtr->setSingleShot(false);
+        connect( m_timerPtr, SIGNAL(timeout()), this, SLOT(startInternal()) );
+        m_timerPtr->start(1000);
+    }
+
+protected slots:
+    void startInternal()
+    {
+        m_counter++;
+        qDebug() << QString("[%1][%2] Step %3")
+                    .arg(QThread::currentThread()->objectName())
+                    .arg(m_workerName)
+                    .arg(m_counter);
+        QThread::msleep( qrand() % 1000 );
+        if ( m_counter == 10  )
         {
-            qDebug() << QString("[%1][%2] Step %3")
-                        .arg(QThread::currentThread()->objectName())
-                        .arg(m_workerName)
-                        .arg(i);
-            QThread::sleep(1);
+            m_timerPtr->stop();
+            emit workerFinishedSignal();
         }
-        emit workerFinishedSignal();
     }
 
 public:
     Worker(const QString& i_name) :
-        m_workerName(i_name)
+        m_workerName(i_name),
+        m_timerPtr(nullptr),
+        m_counter(0)
     {
+        // Do NOT instantiate the QTimer here!
+        qsrand( static_cast<quint32>(QDateTime::currentDateTimeUtc().toMSecsSinceEpoch()) );
+
         qDebug() << QString("[%1][%2] Worker created")
                     .arg(QThread::currentThread()->objectName())
                     .arg(m_workerName);
