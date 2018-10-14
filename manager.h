a39ab7a372ac09a66452607475a85213361df5aa
Sergio Borghese <s.borghese@netresults.it>
2018-10-15 00:03:03 +0200

    one thread and two sync workers

--- manager.h
+++ manager.h
@@ -5,6 +5,7 @@
 #include <QThread>
 #include <QString>
 #include <QDebug>
+#include <QList>
 
 #include "worker.h"
 
@@ -12,30 +13,38 @@ class ThreadManager :public QObject
 {
     Q_OBJECT
 
-    Worker* m_workerPtr;
     QThread m_workerThread;
+    QList<Worker*> m_workerPtrList;
 
 public:
     ThreadManager()
     {
-        // create the worker object
-        m_workerPtr = new Worker("W_FIRST_WORKER");
         // name the worker thread
         m_workerThread.setObjectName("T_WORKER_THREAD");
-        // move worker to thread
-        m_workerPtr->moveToThread(&m_workerThread);
-        // configure signals and slots
-        connect( &m_workerThread, SIGNAL(started()), m_workerPtr, SLOT(start()) );
-        connect( m_workerPtr, SIGNAL(workerFinishedSignal()), m_workerPtr, SLOT(deleteLater()) );
+        // create workers
+        createWorker( "W_FIRST" );
+        createWorker( "W_SECOND" );
     }
 
     ~ThreadManager()
     {
-        if (m_workerPtr) { delete m_workerPtr; }
         qDebug() << "Thread manager destroyed... stopping worker thread";
         m_workerThread.quit();
         m_workerThread.wait();
         qDebug() << "Worker thread terminated correctly";
+    }
+
+    void createWorker(const QString& i_workerName)
+    {
+        // create the worker object
+        Worker* workerPtr = new Worker(i_workerName);
+        // move worker to thread
+        workerPtr->moveToThread(&m_workerThread);
+        // configure signals and slots
+        connect( &m_workerThread, SIGNAL(started()), workerPtr, SLOT(start()) );
+        connect( &m_workerThread, SIGNAL(finished()), workerPtr, SLOT(deleteLater()) );
+        connect( workerPtr, SIGNAL(workerFinishedSignal()), workerPtr, SLOT(deleteLater()) );
+        m_workerPtrList.append(workerPtr);
     }
 
     void start()
